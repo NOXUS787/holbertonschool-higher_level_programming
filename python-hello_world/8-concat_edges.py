3e92e459cfd18606a8307fd82b1794d58658527d
NOXUS787 <alexander.rmn89@gmail.com>
2026-02-16 16:05:49 -0400

    Fix line length and format for task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[39:] + word[:6] + word[6:16] + word[16:24] + word[24:32] + word[32:39])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[107:112] + str[:6]
+print(str)
