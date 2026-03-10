188eef834a032f8e2ec7f5a4c64dcbe17403ecd1
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-10 16:15:45 -0400

    Task 12: Roman to integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) != str:
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+             'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev = 0
+    for c in reversed(roman_string):
+        val = roman.get(c, 0)
+        if val < prev:
+            total -= val
+        else:
+            total += val
+        prev = val
+    return total
