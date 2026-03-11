06223fbc8edddaf81b7cf9cb6d4121112317a027
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-11 14:22:54 -0400

    Task 3: print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for printing a square"""
+
+
+def print_square(size):
+    """Prints a square with #"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)#!/usr/bin/python3
+"""Module for printing a square"""
+
+
+def print_square(size):
+    """Prints a square with #"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
