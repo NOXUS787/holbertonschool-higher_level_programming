78cbe5031a2b2ce3b6818c8a8af6138e67e1e658
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-11 15:38:03 -0400

    Task 1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Square class with private size"""
+
+    def __init__(self, size):
+        """Initialize square with size"""
+        self.__size = size
