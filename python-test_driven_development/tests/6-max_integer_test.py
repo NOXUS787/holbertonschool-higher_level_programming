54c1679a5bf4c0785224bbaa0240012a5e030feb
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-11 15:15:45 -0400

    Task 5: unittest max_integer

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Unittest for max_integer"""
+"""Unittests for max_integer"""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
@@ -14,23 +14,20 @@ class TestMaxInteger(unittest.TestCase):
     def test_single_element(self):
         self.assertEqual(max_integer([5]), 5)
 
-    def test_sorted_list(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_unsorted_list(self):
-        self.assertEqual(max_integer([1, 3, 2, 4]), 4)
-
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-1, -2, -3]), -1)
-
-    def test_mixed_numbers(self):
-        self.assertEqual(max_integer([-10, 5, 3, 2]), 5)
 
     def test_max_at_beginning(self):
         self.assertEqual(max_integer([10, 2, 3, 4]), 10)
 
-    def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 10]), 10)
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([1, 9, 3, 4]), 9)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer([-1, 5, 3]), 5)
+
+    def test_all_negative(self):
+        self.assertEqual(max_integer([-4, -2, -9]), -2)
 
 
 if __name__ == "__main__":
