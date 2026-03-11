30a6896e51f535cf746286135be3e90dccaae86e
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-11 18:12:36 -0400

    Task 7: Integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,6 +13,5 @@ class BaseGeometry:
         """Validate value as a positive integer"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
