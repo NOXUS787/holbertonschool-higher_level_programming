38f5fdb45d9fcd33b48b329cb1c80b8a31d02f69
NOXUS787 <alexander.rmn89@gmail.com>
2026-03-11 15:08:54 -0400

    Task 4: text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for text indentation"""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after ., ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        if text[i] in ".?:":
+            print(text[i])
+            print()
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+        else:
+            print(text[i], end="")
+            i += 1
