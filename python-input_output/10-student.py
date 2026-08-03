330c74049fb297b5335e25d73caae301571cefa9
NOXUS787 <alexander.rmn89@gmail.com>
2026-08-02 20:53:04 -0400

    Add task 10: student to JSON with filter

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Module that defines a Student class"""
+"""Defines a Student class whose dictionary representation can be
+filtered by a list of attribute names."""
 
 
 class Student:
-    """Student class"""
+    """Represents a student with a first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize student attributes"""
+        """Initialize a new Student with the given names and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """
-        Return dictionary representation of the Student.
-        If attrs is a list of strings, only return those attributes.
-        """
-        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            result = {}
-            for attr in attrs:
-                if attr in self.__dict__:
-                    result[attr] = self.__dict__[attr]
-            return result
+        """Retrieve a dictionary representation of the Student instance.
 
+        Args:
+            attrs: an optional list of strings naming the attributes to
+                include. If it is not a list of strings, every attribute
+                is returned.
+
+        Returns:
+            dict: the requested attributes of the instance.
+        """
+        if (type(attrs) is list and
+                all(type(item) is str for item in attrs)):
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
         return self.__dict__
