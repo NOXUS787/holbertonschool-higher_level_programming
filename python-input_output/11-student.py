64fb01867f1771ce8cce1728b1fc126d8d081452
NOXUS787 <alexander.rmn89@gmail.com>
2026-08-02 20:53:46 -0400

    Add task 11: student to disk and reload

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-"""Module that defines a Student class"""
+"""Defines a Student class that can be serialized to a dictionary and
+rebuilt from one, providing a simple serialization mechanism."""
 
 
 class Student:
-    """Student class"""
+    """Represents a student with a first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize student attributes"""
+        """Initialize a new Student with the given names and age."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return dictionary representation of Student"""
-        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            result = {}
-            for attr in attrs:
-                if attr in self.__dict__:
-                    result[attr] = self.__dict__[attr]
-            return result
+        """Retrieve a dictionary representation of the Student instance.
+
+        Args:
+            attrs: an optional list of strings naming the attributes to
+                include. If it is not a list of strings, every attribute
+                is returned.
+
+        Returns:
+            dict: the requested attributes of the instance.
+        """
+        if (type(attrs) is list and
+                all(type(item) is str for item in attrs)):
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
         return self.__dict__
 
     def reload_from_json(self, json):
-        """Replace all attributes of Student instance from dictionary"""
+        """Replace every attribute of the Student instance.
+
+        Args:
+            json: a dictionary whose keys are public attribute names and
+                whose values are the values to assign to them.
+        """
         for key, value in json.items():
             setattr(self, key, value)
