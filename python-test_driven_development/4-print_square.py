7266229ef6d54ab145b5ca964916e018784e17ad
NOXUS787 <alexander.rmn89@gmail.com>
2026-08-02 22:33:42 -0400

    Fix pycodestyle issues in print_square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Module for printing a square"""
+"""This module supplies one function, print_square.
+
+It prints a square of a given size using the # character.
+"""
 
 
 def print_square(size):
-    """Prints a square with #"""
+    """Print a square of # characters with sides of the given size.
 
-    if not isinstance(size, int):
+    Raises a TypeError if size is not an integer and a ValueError if it
+    is negative.
+    """
+    if not isinstance(size, int) or isinstance(size, bool):
         raise TypeError("size must be an integer")
 
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        print("#" * size)#!/usr/bin/python3
-"""Module for printing a square"""
-
-
-def print_square(size):
-    """Prints a square with #"""
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    for i in range(size):
+    for _ in range(size):
         print("#" * size)
